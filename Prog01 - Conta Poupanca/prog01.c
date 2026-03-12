2475f20a324d2ddc1aa6bdf4dc74a49882cfecc2
Ana Sanches <anaclara.leite.sanches@gmail.com>
2026-03-12 16:42:41 -0300

    Initial Commit

---
+++ Prog01 - Conta Poupanca/prog01.c
@@ -0,0 +1,49 @@
+/*
+----------------------------------------------------------------------------------------------
+Prog01 - Conta Poupanca
+Aula: 12/03/2026
+----------------------------------------------------------------------------------------------
+Uma conta poupanca foi aberta com um deposito de R$500.00, com rendimento 1% de juros ao mes. 
+No segundo mes, R$200.00 foram depositados nessa conta poupanca. No terceiro mes, R$50.00 foram 
+retirados da conta. Quanto havera nessa conta no quarto mes?
+*/
+
+#include <stdio.h>
+
+int main(void){
+
+
+	// Primeiro mes
+	float saldo = 500;
+	float juros;
+
+	printf("\nPRIMEIRO MES:\n");
+	printf("Saldo Inicial: R$%.2f", saldo);
+
+	// Segundo mes
+	juros = saldo*1/100; // Juros de 1% ao mes
+	saldo += juros;
+	saldo += 200;
+
+	printf("\n\nSEGUNDO MES:");
+	printf("\nJuros: +R$%.2f", juros);
+	printf("\nEntrada: +R$200.00");
+
+	// Terceiro mes
+	juros = saldo*1/100; // Juros de 1% ao mes
+	saldo += juros;
+	saldo -= 50;
+
+	printf("\n\nTERCEIRO MES:");
+	printf("\nJuros: +R$%.2f", juros);
+	printf("\nSaida: -R$50.00");
+
+	// Quarto mes
+	juros = saldo*1/100; // Juros de 1% ao mes
+	saldo += juros;
+	printf("\n\nQUARTO MES:");
+	printf("\nJuros: +R$%.2f", juros);
+	printf("\nSaldo Final: R$%.2f\n\n", saldo);
+
+	return 0;
+}
