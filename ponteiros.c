43ed20bbc157d863c5ec619da8ba8e41d6fcf281
Ana Sanches <dev.anaclarasanches@gmail.com>
2026-04-09 17:01:01 -0300

    Estudo sobre ponteiros

---
+++ ponteiros.c
@@ -0,0 +1,41 @@
+/*
+PONTEIROS
+
+O que é um ponteiro?
+É uma "variável referência" que guarda o endereço de memória para outra variável
+
+Vantagens de usar ponteiros
+1. Menos tempo de execução do programa
+2. Você trabalha com a variável original
+3. É possível criar outras estruturas de dados com a ajuda de ponteiros (listas conectadas, pilha, fila)
+4. Retornar mais de um valor de funções
+5. Procurar e selecionar em grande fonte de dados mais facilmente
+6. Alocação dinâmica de memória
+
+Notações:
+- (*): Retorna o conteúdo da variável (Exemplo: *var)
+- (&): Retorna o endereço da variável (Exemplo: &var)
+
+*/
+
+#include <stdio.h>
+
+void printAge(int *age){
+	printf("Valor de *age: %d", *age);
+}
+
+int main(){
+
+	int age = 21;
+	int *pAge = NULL; // Boa pratica
+	pAge = &age;
+
+	printf("Endereco de age: %p\n", &age);
+	printf("Valor de age: %d\n", age);
+	printf("Valor de pAge: %p\n", pAge);
+	printf("Valor no endereco guardado em pAge: %d\n", *pAge);
+ 	
+ 	printAge(pAge);
+
+	return 0;
+}
